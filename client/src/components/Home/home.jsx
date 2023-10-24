3baa51ba4ec5155598fcbfef2426045704f008b0
Villanueva-Flavio <fvillanueva@fi.uba.ar>
2023-10-24 11:52:02 -0300

    [ADD] Archivos, falta home y sidebar

--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './home.module.css'
+
 
 const home = () => {
   return (
-    <div>home</div>
+    <div>
+
+    </div>
   )
 }
 
