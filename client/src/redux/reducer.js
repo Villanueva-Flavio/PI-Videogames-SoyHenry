f6a3a2f9c7c94426aa67f74c82a56e993e7191c0
Villanueva-Flavio <fvillanueva@fi.uba.ar>
2023-10-25 11:50:24 -0300

    [FIX] Style searchbar

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -14,7 +14,6 @@ export default function reducer ( state = initialState, {type, payload}){
     switch(type){
 
 
-        
         case SEARCH_BY_NAME:{
             return{
                 ...state,
